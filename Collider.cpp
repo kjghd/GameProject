8a2d8397222941b16ecf5ce204f9aa15a8be3668
Goblin <no email>
2022-12-07 20:38:43 +1000

    Classes to handle collision.

---
+++ Collider.cpp
@@ -0,0 +1,169 @@
+#include "Collider.h"
+
+#include <cmath>
+#include <algorithm>
+
+// Collider
+Collider::Collider(game::Float2* pOrigin, bool dyn, bool blck)
+	:
+	origin(*pOrigin),
+	dynamic(dyn),
+	block(blck)
+{
+}
+
+void Collider::CheckCollision(Collider* pCollider)
+{
+
+}
+
+
+// Collider Box
+Collider_Box::Collider_Box(game::Float2* pOrigin, game::Float2 sz, bool dyn, bool blck)
+	:
+	Collider(pOrigin, dyn, blck),
+	size(sz)
+{
+}
+Collider_Box::Collider_Box(game::Float2* pOrigin, const Collider_Box& collider)
+	:
+	Collider(pOrigin, collider.dynamic, collider.block),
+	size(collider.size)
+{
+}
+
+void Collider_Box::CheckCollision(Collider* pCollider)
+{
+	// Box vs circle
+	if (dynamic_cast<Collider_Circle*>(pCollider))
+	{
+		Collider_Circle* circle{ dynamic_cast<Collider_Circle*>(pCollider) };
+
+		// Closest part of box to cicle.
+		game::Float2 point{
+		std::clamp<float>(
+			circle->origin.x,
+			origin.x - size.x / 2,
+			origin.x + size.x / 2
+		),
+		std::clamp<float>(
+			circle->origin.y,
+			origin.y - size.y / 2,
+			origin.y + size.y / 2
+		),
+		};
+
+		// Vector from box point to circle center.
+		game::Float2 vec{ circle->origin - point };
+
+		float length = std::sqrtf(std::powf(vec.x, 2.f) + std::powf(vec.y, 2.f));
+		float overlap = circle->radius - length;
+
+		if (overlap >= 0)
+		{
+			if (block && circle->block)
+			{
+				if (dynamic) origin -= vec * (overlap / length);
+				if (pCollider->dynamic) pCollider->origin += vec * (overlap / length);
+			}
+		}
+	}
+	// Box vs box
+	else if (dynamic_cast<Collider_Box*>(pCollider))
+	{
+		Collider_Box* box{ dynamic_cast<Collider_Box*>(pCollider) };
+
+		// Object A (this)
+		float at = origin.y + size.y / 2;
+		float ab = origin.y - size.y / 2;
+		float al = origin.x - size.x / 2;
+		float ar = origin.x + size.x / 2;
+		// Object B (target)
+		float bt = box->origin.y + box->size.y / 2;
+		float bb = box->origin.y - box->size.y / 2;
+		float bl = box->origin.x - box->size.x / 2;
+		float br = box->origin.x + box->size.x / 2;
+
+		// Overlap
+		float t = at - bb;
+		float b = bt - ab;
+		float l = br - al;
+		float r = ar - bl;
+		// Shortest overlap
+		float shortest_y = t < b ? t : b;
+		float shortest_x = l < r ? l : r;
+		float shortest = shortest_x < shortest_y ? shortest_x : shortest_y;
+
+		if (shortest >= 0)
+		{
+			if (block && box->block)
+			{
+				if (dynamic)
+				{
+					if (t == shortest) origin.y -= t;
+					if (b == shortest) origin.y += b;
+					if (l == shortest) origin.x += l;
+					if (r == shortest) origin.x -= r;
+				}
+				if (pCollider->dynamic)
+				{
+					if (t == shortest) pCollider->origin.y += t;
+					if (b == shortest) pCollider->origin.y -= b;
+					if (l == shortest) pCollider->origin.x -= l;
+					if (r == shortest) pCollider->origin.x += r;
+				}
+			}
+		}
+	}
+};
+
+
+// Collider Circle
+Collider_Circle::Collider_Circle(game::Float2* pOrigin, float r, bool dyn, bool blck)
+	:
+	Collider(pOrigin, dyn, blck),
+	radius(r)
+{
+}
+
+void Collider_Circle::CheckCollision(Collider* pCollider)
+{
+	// Circle vs circle
+	if (dynamic_cast<Collider_Circle*>(pCollider))
+	{
+		Collider_Circle* circle{ dynamic_cast<Collider_Circle*>(pCollider) };
+
+	}
+	// Circle vs box
+	else if (dynamic_cast<Collider_Box*>(pCollider))
+	{
+		Collider_Box* box{ dynamic_cast<Collider_Box*>(pCollider) };
+
+		game::Float2 point{
+		std::clamp<float>(
+			origin.x,
+			box->origin.x - box->size.x / 2,
+			box->origin.x + box->size.x / 2
+		),
+		std::clamp<float>(
+			origin.y,
+			box->origin.y - box->size.y / 2,
+			box->origin.y + box->size.y / 2
+		)
+		};
+
+		game::Float2 vec{ point - origin };
+
+		float length = std::sqrtf(std::powf(vec.x, 2.f) + std::powf(vec.y, 2.f));
+		float overlap = radius - length;
+
+		if (overlap >= 0)
+		{
+			if (block && box->block)
+			{
+				if (dynamic) origin -= vec * (overlap / length);
+				if (pCollider->dynamic) pCollider->origin += vec * (overlap / length);
+			}
+		}
+	}
+};
---
+++ Collider.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "DataTypes.h"
+
+class Collider
+{
+public:
+	game::Float2& origin;
+	bool dynamic;
+	bool block;
+
+	Collider(game::Float2* pOrigin, bool dyn = true, bool blck = true);
+
+	virtual void CheckCollision(Collider* pCollider);
+};
+class Collider_Box : public Collider
+{
+public:
+	game::Float2 size;
+
+	Collider_Box(game::Float2* pOrigin, game::Float2 sz, bool dyn = true, bool blck = true);
+	Collider_Box(game::Float2* pOrigin, const Collider_Box& collider);
+
+	virtual void CheckCollision(Collider* pCollider) override;
+
+	Collider_Box& operator =(const Collider_Box& collider)
+	{
+		origin = collider.origin;
+		dynamic = collider.dynamic;
+		block = collider.block;
+		size = collider.size;
+		return *this;
+	}
+};
+class Collider_Circle : public Collider
+{
+public:
+	float radius;
+
+	Collider_Circle(game::Float2* pOrigin, float r, bool dyn = true, bool blck = true);
+
+	virtual void CheckCollision(Collider* pCollider) override;
+};
