a23d9f4af4468fa191d517153cadc82352d516cf
James <jamesw.0360@gmail.com>
2022-12-10 21:26:57 +1000

    Organised enumerations

--- PrefabTags.h
+++ PrefabTags.h
@@ -3,9 +3,6 @@
 
 enum PrefabTag
 {
-	PREFAB_Block_Brick_01,
-	PREFAB_Block_Pavement_01,
-	
 	PREFAB_Block_Dynamic,
 	PREFAB_Block_Wall,
 	PREFAB_Block_Floor,
--- Textures.h
+++ Textures.h
@@ -4,14 +4,15 @@
 enum Textures
 {
 	T_Error,
+
 	T_Shadow,
+	T_Wallace,
 
 	T_Blue,
 	T_Green,
 	T_Red,
 	T_Yellow,
 
-	T_Wallace,
 
 	T_COUNT
 };
